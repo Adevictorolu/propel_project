a07de81eac6a424940e54b8e0271a9ea30c7af65
Ademola Victor Oluokun <137506855+Adevictorolu@users.noreply.github.com>
2025-06-16 02:34:22 +0100

    Added Now

--- lib/mentorsdetails.dart
+++ lib/mentorsdetails.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:propel_project/colors.dart';
 
 class MentorDetails extends StatefulWidget {
   const MentorDetails({super.key});
@@ -19,11 +20,87 @@ class _MentorDetailsState extends State<MentorDetails> {
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
           ),
           Gap(10),
-          Container(
+          SizedBox(
             width: double.infinity,
-            height: 400,
-            child: Image(image: AssetImage('asset/img/469717357_18029294267526943_1835797228880026636_n.jpg'),),
-          )
+            height: 300,
+            child: Image(
+              image: AssetImage(
+                'asset/img/469717357_18029294267526943_1835797228880026636_n.jpg',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 8, left: 8),
+            child: Container(
+              width: double.infinity,
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                border: Border.all(width: 1, color: Colors.grey),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Mr Akinakingbogun',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Gap(2),
+                            Text(
+                              'Mentor for Personal Development',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 1),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                          height: 40,
+                          width: 90,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                color: Colorspage.buttoncolor,
+                                onPressed: () {},
+                                icon: Icon(Icons.message_rounded),
+                              ),
+                              Gap(2),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.workspaces_filled,
+                                  color: Colorspage.buttoncolor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
